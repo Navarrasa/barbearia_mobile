840bbb6d0f29664cf5f0a9c2104838970d01c03a
Gustavo Bruno <gugubrunopf@gmail.com>
2025-11-18 11:37:10 -0300

    feat: new mobile app

---
+++ barbearia/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyBarberShop());
+
+class MyBarberShop extends StatelessWidget {
+  const MyBarberShop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      
+
+    );
+  }
+}
